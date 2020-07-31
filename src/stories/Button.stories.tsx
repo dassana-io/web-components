d0aad12f937d3f8b4bf45509b415ec804ba542ae
sam-m-m <sam@dassana.io>
2020-07-30 19:29:35 -0700

    Add button stories

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,58 @@
+import { action } from '@storybook/addon-actions'
+import React from 'react'
+import Button from '../components/Button'
+
+export default {
+	title: 'Button',
+	component: Button,
+	excludeStories: /.*Data$/
+}
+
+export const actionsData = {
+	onClick: action('onClick')
+}
+
+export const Default = () => <Button {...actionsData}>Default</Button>
+
+export const disabledData = {
+	disabled: true
+}
+
+export const Disabled = () => (
+	<Button {...actionsData} {...disabledData}>
+		Disabled
+	</Button>
+)
+
+export const primaryData = {
+	primary: true
+}
+
+export const Primary = () => (
+	<Button {...actionsData} {...primaryData}>
+		Primary
+	</Button>
+)
+
+export const primaryDisabledData = {
+	primary: true,
+	disabled: true
+}
+
+export const PrimaryDisabled = () => (
+	<Button {...actionsData} {...primaryDisabledData}>
+		Primary Disabled
+	</Button>
+)
+
+export const googleData = {
+	classes: 'google plus',
+	primary: false,
+	text: 'Google'
+}
+
+export const Google = () => (
+	<Button {...actionsData} {...googleData}>
+		Google
+	</Button>
+)
