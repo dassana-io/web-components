840ccdb83712175b2b30a8e9524e67eb2ca25b6d
sam-m-m <sam@dassana.io>
2020-07-31 10:30:50 -0700

    Add Button tests

---
+++ src/tests/Button.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Button from '../components/Button'
+import { shallow, ShallowWrapper } from 'enzyme'
+
+let wrapper: ShallowWrapper
+let mockClick: jest.Mock<any, any>
+
+beforeEach(() => {
+	mockClick = jest.fn()
+	wrapper = shallow(<Button onClick={mockClick} />)
+})
+
+describe('<Button />', () => {
+	it('renders', () => {
+		expect(wrapper.find('button')).toHaveLength(1)
+	})
+
+	it('runs onClick function when button is clicked', () => {
+		expect(wrapper.find('button').simulate('click'))
+		expect(mockClick).toHaveBeenCalled()
+		expect(mockClick).toHaveBeenCalledTimes(1)
+	})
+})
