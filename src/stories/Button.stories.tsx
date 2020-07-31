723ec0c33ddf4aab65efbbba05fd71b6e0d9a5a9
sam-m-m <sam@dassana.io>
2020-07-31 16:49:19 -0700

    Update Button.stories.tsx

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -22,27 +22,32 @@ export const buttonData: ButtonProps = {
 
 export const Default = () => <Button {...buttonData}>Default</Button>
 
-export const Disabled = () => (
-	<Button {...{ ...buttonData, disabled: true }}>Disabled</Button>
-)
+export const Disabled = () => {
+	const props: ButtonProps = { ...buttonData, disabled: true }
+	return <Button {...props}>Disabled</Button>
+}
 
-export const Primary = () => (
-	<Button {...{ ...buttonData, primary: true }}>Primary</Button>
-)
+export const Primary = () => {
+	const props: ButtonProps = { ...buttonData, primary: true }
+	return <Button {...props}>Primary</Button>
+}
 
-export const Submit = () => (
-	<Button {...{ ...buttonData, type: 'submit' }}>Submit</Button>
-)
+export const Submit = () => {
+	const props: ButtonProps = { ...buttonData, type: 'submit' }
+	return <Button {...props}>Submit</Button>
+}
 
-export const PrimaryDisabled = () => (
-	<Button {...{ ...buttonData, primary: true, disabled: true }}>
-		Primary Disabled
-	</Button>
-)
+export const PrimaryDisabled = () => {
+	const props: ButtonProps = { ...buttonData, primary: true, disabled: true }
+	return <Button {...props}>Primary Disabled</Button>
+}
 
-export const Google = () => (
-	<Button {...{ ...buttonData, classes: ['red'] }}>
-		<i className='google icon'></i>
-		Google
-	</Button>
-)
+export const Google = () => {
+	const props: ButtonProps = { ...buttonData, classes: ['red'] }
+	return (
+		<Button {...props}>
+			<i className='google icon'></i>
+			Google
+		</Button>
+	)
+}
