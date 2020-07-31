50394b4fd51b2646a2ee37c6bff26650beda1ce3
sam-m-m <sam@dassana.io>
2020-07-31 15:10:44 -0700

    Remove semantic-ui dependency from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import App from 'components/App'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'semantic-ui-css/semantic.css'
 
 ReactDOM.render(<App />, document.querySelector('#root'))
