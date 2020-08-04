a1217d9d948575c3d345e993d22dc1a35cf3b4e7
sam-m-m <sam@dassana.io>
2020-08-04 16:31:52 -0700

    feat #26 - Add and config @storybook/addon-docs

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,6 @@
-import { addDecorator } from '@storybook/react'
+import { addDecorator, addParameters } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
+import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks'
 import 'semantic-ui-css/semantic.css'
 import React, { ReactNode } from 'react'
 
@@ -7,11 +8,19 @@ const infoOptions = {
 	header: false,
 	source: true,
 	propTables: false
+	// inline: true //uncomment for "story source" to be rendered inline
 }
 
 const storyWrapper = (story: () => ReactNode) => (
 	<div style={{ padding: 10 }}>{story()}</div>
 )
+
+addParameters({
+	docs: {
+		container: DocsContainer,
+		page: DocsPage
+	}
+})
 
 addDecorator(withInfo(infoOptions))
 
