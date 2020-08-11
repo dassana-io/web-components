5b70e2459a40d125c5f7a6ada49a9802b9428a9a
Sam M <68707128+sam-m-m@users.noreply.github.com>
2020-08-11 16:05:55 -0700

    feat #38 - Add eslint and fix lint errors & warnings (#39)
    * Feat #38 - Add typescript Eslint plugins & .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,68 @@
+module.exports = {
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended' // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+	],
+	globals: {
+		console: true,
+		localStorage: true,
+		module: true,
+		window: true
+	},
+	parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+			modules: true
+		},
+		ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+		sourceType: 'module', // Allows for the use of imports
+		project: './tsconfig.json'
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+		// e.g. "@typescript-eslint/explicit-function-return-type": "off",
+		'@typescript-eslint/ban-types': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-extra-semi': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars-experimental': 'warn',
+		'comma-dangle': ['warn', 'never'],
+		'comma-spacing': ['warn', { after: true, before: false }],
+		'key-spacing': [
+			'warn',
+			{
+				afterColon: true,
+				beforeColon: false
+			}
+		],
+		'no-duplicate-imports': 'error',
+		'no-useless-computed-key': 'warn',
+		'quote-props': ['warn', 'as-needed'],
+		quotes: ['warn', 'single'],
+		'react/display-name': 'off',
+		'react/jsx-sort-props': 'warn',
+		semi: ['warn', 'never'],
+		'semi-spacing': ['warn', { after: true, before: false }],
+		'sort-imports': [
+			'warn',
+			{
+				ignoreCase: true,
+				ignoreMemberSort: false,
+				memberSyntaxSortOrder: ['none', 'all', 'single', 'multiple']
+			}
+		],
+		'sort-keys': ['warn', 'asc'],
+		'space-in-parens': ['warn', 'never']
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	}
+}
