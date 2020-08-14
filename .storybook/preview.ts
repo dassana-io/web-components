1410110606def72115c10f33c57139892c1a0d95
sam-m-m <sam@dassana.io>
2020-08-14 13:04:00 -0700

    Chore #49 - Update .storybook/preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,5 +4,18 @@ export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
 	viewport: {
 		viewports: INITIAL_VIEWPORTS
+	},
+	backgrounds: {
+		default: 'dark',
+		values: [
+			{
+				name: 'dark',
+				value: '#212121'
+			},
+			{
+				name: 'light',
+				value: '#ffffff'
+			}
+		]
 	}
 }
