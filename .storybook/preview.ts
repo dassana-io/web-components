9368b414c33c8af505d0fa1d2accd70fc3d15d9d
sam-m-m <sam@dassana.io>
2020-08-14 15:26:54 -0700

    Chore #49 - Add comment to preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@ import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
+	// controls: { expanded: true }, // uncomment to display controls with description and defaults
 	viewport: {
 		viewports: INITIAL_VIEWPORTS
 	},
