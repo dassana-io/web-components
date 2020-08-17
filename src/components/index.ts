f013175b9eada0fdc75a863d21a3491e59801b10
sam-m-m <sam@dassana.io>
2020-08-17 16:55:06 -0700

    Chore #49 - Fix bug styles import

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 import 'normalize.css'
-import '../styles'
+import '../styles/index.css'
 export { default as Button } from './Button'
 export { default as Link } from './Link'
 export { default as Tag } from './Tag'
