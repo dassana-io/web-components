8ed4189d9cf706112760239f24c7d9e78f00ca0f
Sam M <68707128+sam-m-m@users.noreply.github.com>
2020-08-18 13:46:15 -0700

    Chore #49- Upgrade Storybook and dependencies (#50)
    * Chore #49 - Upgrade sb and deps to 6.0.10
    
    * Chore #49 - Bump up version 0.1.7 --> 0.1.8
    
    * Chore #49 - Add react-jss & update linkColor
    
    * Chore #49 - Add normalize.css
    
    Co-authored-by: Parth Shah <parth@dassana.io>

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -3,7 +3,6 @@ import { Tag as AntDTag } from 'antd'
 import React, { FC, ReactNode } from 'react'
 
 export interface TagProps {
-<<<<<<< HEAD
 	/**
 	 * Tag children to render including tag text.
 	 */
@@ -11,9 +10,6 @@ export interface TagProps {
 	/**
 	 * Color of tag - either a preset (`red`, `blue`, `green` etc.), a hex color code(eg. `#ffffff`) or a rgb color value(eg. `rgb(255, 0, 0)`).
 	 */
-=======
-	children: ReactNode
->>>>>>> Feat #43 - Tag, Link components
 	color?: string
 }
 
