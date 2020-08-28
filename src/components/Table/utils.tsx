5abe3a4d897bf280d9d7aa9a95a7549524af8b9a
sam-m-m <sam@dassana.io>
2020-08-28 10:48:30 -0700

    feat #62 - Refac Table/utils

--- src/components/Table/utils.tsx
+++ src/components/Table/utils.tsx
@@ -8,6 +8,107 @@ import React, { Key } from 'react'
 import Tag, { TagProps } from '../Tag'
 import Toggle, { ToggleProps } from '../Toggle'
 
+/* ------- Exported Functions ------- */
+
+/* Takes columns prop passed to Table and returns columns
+formatted to satisfy antD requirements. */
+export function processColumns<DataType>(columns: ColumnType[]) {
+	const processedColumns: AntDColumnType<DataType>[] = []
+
+	for (const column of columns) {
+		const { dataIndex, format, title, sort, type } = column
+		const antDColumn: AntDColumnType<DataType> = {
+			dataIndex,
+			showSorterTooltip: false,
+			title
+		}
+
+		type DataFormatType = typeof format
+		type DataTypeType = typeof type
+
+		applyRender<DataTypeType, DataFormatType, DataType>(
+			type,
+			format,
+			column,
+			antDColumn
+		)
+
+		if (sort !== false) {
+			applySort<DataTypeType, DataFormatType, DataType>(
+				type,
+				format,
+				column,
+				antDColumn
+			)
+		}
+
+		processedColumns.push(antDColumn)
+	}
+
+	return processedColumns
+}
+
+/*
+Takes data prop passed to Table and returns data:
+  1. formatted to satisfy antD requirements
+  2. with an added _FORMATTED_DATA key and array of formatted data value
+    (this makes rows searchable by formatted data).
+  */
+export function processData<DataType>(data: DataType[], columns: ColumnType[]) {
+	const processedData: DataType[] = []
+	const mappedFormat = mapDataIndexToFormatter(columns)
+	let i: Key = 0
+
+	for (const item of data) {
+		const _FORMATTED_DATA = createFormattedData(mappedFormat, item)
+
+		const row = {
+			...item,
+			_FORMATTED_DATA,
+			key: i++
+		}
+
+		processedData.push(row)
+	}
+
+	return processedData
+}
+
+/*
+Maps keys from columns - the values of which should be searched or filtered on.
+This will be used for "global" search using fuse.
+More info --> https://fusejs.io/examples.html#nested-search
+ */
+export function mapFilterKeys(columns: ColumnType[]) {
+	const keysArr: (string | string[])[] = ['_FORMATTED_DATA']
+
+	for (const column of columns) {
+		const { dataIndex, format, type } = column
+
+		if (type === 'component') {
+			switch (format) {
+				case 'tag':
+					keysArr.push([dataIndex, 'children'])
+					break
+
+				case 'link':
+					keysArr.push([dataIndex, 'children'])
+					break
+
+				case 'icon':
+					keysArr.push([dataIndex, 'icon'], [dataIndex, 'iconKey'])
+					break
+			}
+		} else if (type === 'string') {
+			keysArr.push(dataIndex)
+		} else if (type === 'number') {
+			keysArr.push(dataIndex)
+		}
+	}
+
+	return keysArr
+}
+
 /* -*-*-*-*-*- Helpers for parsing columns -*-*-*-*-*- */
 
 /* -x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
@@ -133,15 +234,15 @@ function createFormattedData<DataType>(
 	mappedFormat: Record<string, FormatterFnType>,
 	item: DataType
 ) {
-	const FORMATTED_DATA = []
+	const _FORMATTED_DATA = []
 
 	for (const key of Object.keys(item)) {
 		if (key in mappedFormat)
 			// @ts-ignore
-			FORMATTED_DATA.push(mappedFormat[key](item[key]))
+			_FORMATTED_DATA.push(mappedFormat[key](item[key]))
 	}
 
-	return FORMATTED_DATA
+	return _FORMATTED_DATA
 }
 
 /* Maps dataIndex to formatter function. E.g. { dateOfBirth: DATE_FORMATTER_FN } */
@@ -180,104 +281,3 @@ function createDateFormatter(column: ColumnType): FormatterFnType {
 
 /* ------- Extracted Types ------- */
 type FormatterFnType = (num: number) => string
-
-/* ------- Exported Functions ------- */
-
-/* Takes columns prop passed to Table and returns columns
-formatted it to satisfy antD requirements. */
-export function processColumns<DataType>(columns: ColumnType[]) {
-	const processedColumns: AntDColumnType<DataType>[] = []
-
-	for (const column of columns) {
-		const { dataIndex, format, title, sort, type } = column
-		const antDColumn: AntDColumnType<DataType> = {
-			dataIndex,
-			showSorterTooltip: false,
-			title
-		}
-
-		type DataFormatType = typeof format
-		type DataTypeType = typeof type
-
-		applyRender<DataTypeType, DataFormatType, DataType>(
-			type,
-			format,
-			column,
-			antDColumn
-		)
-
-		if (sort !== false) {
-			applySort<DataTypeType, DataFormatType, DataType>(
-				type,
-				format,
-				column,
-				antDColumn
-			)
-		}
-
-		processedColumns.push(antDColumn)
-	}
-
-	return processedColumns
-}
-
-/*
-Takes data prop passed to Table and returns data:
-  1. formatted to satisfy antD requirements
-  2. with an added FORMATTED_DATA key and array of formatted data value
-    (this makes rows searchable by formatted data).
-  */
-export function processData<DataType>(data: DataType[], columns: ColumnType[]) {
-	const processedData: DataType[] = []
-	const mappedFormat = mapDataIndexToFormatter(columns)
-	let i: Key = 0
-
-	for (const item of data) {
-		const FORMATTED_DATA = createFormattedData(mappedFormat, item)
-
-		const row = {
-			...item,
-			FORMATTED_DATA,
-			key: i++
-		}
-
-		processedData.push(row)
-	}
-
-	return processedData
-}
-
-/*
-Maps keys from columns - the values of which should be searched or filtered on.
-This will be used for "global" search using fuse.
-More info --> https://fusejs.io/examples.html#nested-search
- */
-export function mapFilterKeys(columns: ColumnType[]) {
-	const keysArr: (string | string[])[] = ['FORMATTED_DATA']
-
-	for (const column of columns) {
-		const { dataIndex, format, type } = column
-
-		if (type === 'component') {
-			switch (format) {
-				case 'tag':
-					keysArr.push([dataIndex, 'children'])
-					break
-
-				case 'link':
-					keysArr.push([dataIndex, 'children'])
-					break
-
-				case 'icon':
-					keysArr.push([dataIndex, 'icon'], [dataIndex, 'iconKey'])
-					break
-			}
-		} else if (type === 'string') {
-			keysArr.push(dataIndex)
-		} else if (type === 'number') {
-			keysArr.push(dataIndex)
-		}
-	}
-
-	return keysArr
-}
