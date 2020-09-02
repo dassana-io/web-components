c16c5a2d2c7e2847108002a567785b19732542ee
sam-m-m <sam@dassana.io>
2020-09-01 17:22:54 -0700

    feat #62 - Add checked prop to Toggle

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -2,34 +2,38 @@ import 'antd/lib/switch/style/index.css'
 import { Switch } from 'antd'
 import React, { FC } from 'react'
 
-export type ToggleSizeType = 'default' | 'small'
-
 export interface ToggleProps {
 	/**
-	 * Required change handler.
+	 * Required change handler
 	 */
 	onChange: () => void
 	/**
-	 * Whether switch will be checked or "on" by default.
+	 * Determines whether the Switch is checked
+	 */
+	checked: boolean
+	/**
+	 * Whether switch will be checked or "on" by default
 	 */
 	defaultChecked?: boolean
 	/**
-	 * Whether switch will be disabled.
+	 * Whether switch will be disabled
 	 */
 	disabled?: boolean
 	/**
-	 * The size of the toggle.
+	 * The size of the toggle
 	 */
-	size?: ToggleSizeType
+	size?: 'default' | 'small'
 }
 
 const Toggle: FC<ToggleProps> = ({
+	checked,
 	onChange,
 	defaultChecked = false,
 	disabled = false,
 	size = 'default'
 }: ToggleProps) => {
 	const antDProps = {
+		checked,
 		defaultChecked,
 		disabled,
 		onChange,
