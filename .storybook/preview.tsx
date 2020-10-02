43edc81ff2f98a19b336a33921c5a2adde48f35a
sam-m-m <sam@dassana.io>
2020-10-02 12:36:56 -0700

    feat #92 - Add side-by-side theming views

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,59 +1,57 @@
-// import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
-// import './index.css'
-import { StoryContext } from '@storybook/addons'
-import { Story } from '@storybook/react/types-6-0'
-// import React from 'react'
-
-// export const parameters = {
-// 	actions: { argTypesRegex: '^on.*' }
-// }
-
-// export const globalTypes = {
-// 	viewport: {
-// 		viewports: INITIAL_VIEWPORTS
-// 	},
-// 	theme: {
-// 		name: 'Theme',
-// 		description: 'Global theme for components',
-// 		defaultValue: 'light',
-// 		toolbar: {
-// 			items: [
-// 				{ value: 'light', title: 'light', icon: 'circlehollow' },
-// 				{ value: 'dark', title: 'dark', icon: 'circle' }
-// 			]
-// 		}
-// 	}
-// }
-
-// export const withThemeProvider = (
-// 	ComponentStory: Story,
-// 	context: StoryContext
-// ) => {
-// 	return (
-// 		<div className={context.globals.theme}>
-// 			<ComponentStory {...context} />
-// 		</div>
-// 	)
-// }
-
-// export const decorators = [withThemeProvider]
-
-// import { document } from 'global'
-var document = require('global/document')
-
+import './index.css'
+import cn from 'classnames'
+import { createUseStyles } from 'react-jss'
+import document from 'global/document'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import isChromatic from 'chromatic/isChromatic'
-import React, { useEffect } from 'react'
+import { Story } from '@storybook/react/types-6-0'
+import { StoryContext } from '@storybook/addons'
+import { withCssResources } from '@storybook/addon-cssresources'
 import {
+	convert,
+	createReset,
 	Global,
+	styled,
+	Theme,
 	ThemeProvider,
 	themes,
-	createReset,
-	convert,
-	useTheme,
-	Theme
+	useTheme
 } from '@storybook/theming'
-import { withCssResources } from '@storybook/addon-cssresources'
-import { DocsPage } from '@storybook/addon-docs/blocks'
+import React, { FC, ReactNode, useEffect } from 'react'
+
+const useStyles = createUseStyles({
+	storyContainer: {
+		display: 'flex'
+	},
+	storyWrapper: {
+		padding: '1rem'
+	}
+})
+
+const ThemeBlock = styled.div(
+	{
+		height: '100vh',
+		left: 0,
+		overflow: 'auto',
+		right: '50vw',
+		width: '50vw'
+	},
+	({ theme }) => ({
+		background: theme.background.app,
+		color: theme.color.defaultText
+	}),
+	// @ts-ignore
+	({ side }) =>
+		side === 'left'
+			? {
+					left: 0,
+					right: '50vw'
+			  }
+			: {
+					left: '50vw',
+					right: 0
+			  }
+)
 
 const ThemedSetRoot = () => {
 	const theme: Theme = useTheme()
@@ -61,68 +59,99 @@ const ThemedSetRoot = () => {
 	useEffect(() => {
 		document.body.style.background = theme.background.app
 		document.body.style.color = theme.defaultText
-
-		theme.base === 'dark'
-			? document.body.classList.add('dark')
-			: document.body.classList.remove('dark')
-
-		return () => {
-			//
-		}
 	})
 
 	return null
 }
 
-export const decorators = [
-	withCssResources,
-	(ComponentStory: Story, { globals: { theme = 'light' } }: StoryContext) => {
-		switch (theme) {
-			default: {
-				return (
-					<ThemeProvider theme={convert(themes[theme])}>
-						<Global styles={createReset} />
-						<ThemedSetRoot />
-						<ComponentStory />
-					</ThemeProvider>
-				)
-			}
-		}
-	}
-]
+interface StoryWrapperProps {
+	children: ReactNode
+	dark?: boolean
+}
+/*
+This wrapper does two things:
+  1. Adds padding to the story since it was removed from .sb-show-main in ./index.css
+  2. Toggles 'dark' theme class
+*/
+const StoryWrapper: FC<StoryWrapperProps> = ({
+	children,
+	dark = false
+}: StoryWrapperProps) => {
+	const classes = useStyles()
+	const wrapperClasses = cn({
+		dark,
+		[classes.storyWrapper]: true
+	})
 
-export const parameters = {
-	exportedParameter: 'exportedParameter',
-	a11y: {
-		config: {},
-		options: {
-			checks: { 'color-contrast': { options: { noScroll: true } } },
-			restoreScroll: true
+	return <div className={wrapperClasses}>{children}</div>
+}
+
+const ThemeWrapper = (
+	ComponentStory: Story,
+	{ globals: { theme = 'light' } }: StoryContext
+) => {
+	const classes = useStyles()
+
+	switch (theme) {
+		case 'side-by-side': {
+			return (
+				<div className={classes.storyContainer}>
+					<ThemeProvider theme={convert(themes.light)}>
+						<Global styles={createReset} />
+					</ThemeProvider>
+					<ThemeProvider theme={convert(themes.light)}>
+						{/* @ts-ignore */}
+						<ThemeBlock side='left'>
+							<StoryWrapper>{<ComponentStory />}</StoryWrapper>
+						</ThemeBlock>
+					</ThemeProvider>
+					<ThemeProvider theme={convert(themes.dark)}>
+						{/* @ts-ignore */}
+						<ThemeBlock side='right'>
+							<StoryWrapper dark={true}>
+								{<ComponentStory />}
+							</StoryWrapper>
+						</ThemeBlock>
+					</ThemeProvider>
+				</div>
+			)
 		}
-	},
-	options: {
-		storySort: (a: any, b: any) =>
-			a[1].kind === b[1].kind
-				? 0
-				: a[1].id.localeCompare(b[1].id, undefined, { numeric: true })
-	},
-	docs: {
-		theme: themes.light,
-		page: () => <DocsPage />
+
+		default: {
+			return (
+				<ThemeProvider theme={convert(themes[theme])}>
+					<Global styles={createReset} />
+					<ThemedSetRoot />
+					<StoryWrapper dark={theme === 'dark'}>
+						{<ComponentStory />}
+					</StoryWrapper>
+				</ThemeProvider>
+			)
+		}
 	}
 }
 
 export const globalTypes = {
 	theme: {
-		name: 'Theme',
+		defaultValue: isChromatic() ? 'side-by-side' : 'light',
 		description: 'Global theme for components',
-		defaultValue: isChromatic() ? 'stacked' : 'light',
+		name: 'Theme',
 		toolbar: {
 			icon: 'circlehollow',
 			items: [
-				{ value: 'light', icon: 'circlehollow', title: 'light' },
-				{ value: 'dark', icon: 'circle', title: 'dark' }
+				{ icon: 'circlehollow', title: 'light', value: 'light' },
+				{ icon: 'circle', title: 'dark', value: 'dark' },
+				{
+					icon: 'sidebar',
+					title: 'side by side',
+					value: 'side-by-side'
+				}
 			]
 		}
+	},
+	viewport: {
+		viewports: INITIAL_VIEWPORTS
 	}
 }
+
+export const decorators = [withCssResources, ThemeWrapper]
