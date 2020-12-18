b8d84d1c0243e8365f07e8a3dc402eeb8ce345b6
sam-m-m <sam@dassana.io>
2020-12-17 20:47:21 -0800

    feat #178 - Refac to include Checkboxes

--- src/components/MultiSelect/MultiSelect.stories.tsx
+++ src/components/MultiSelect/MultiSelect.stories.tsx
@@ -1,26 +1,45 @@
 import { action } from '@storybook/addon-actions'
-import React from 'react'
+import { SbTheme } from '../../../.storybook/preview'
+import { useTheme } from 'react-jss'
 import { Meta, Story } from '@storybook/react/types-6-0'
-import { MultiSelect, MultiSelectOptions, MultiSelectProps } from './index'
+import { MultiSelect, MultiSelectOption, MultiSelectProps } from './index'
+import React, { FC } from 'react'
 
 export default {
 	argTypes: {
 		onChange: { defaultValue: action('onChange') },
-		value: { control: { disable: true } }
+		popupContainerSelector: { control: { disable: true } },
+		values: { control: { disable: true } }
 	},
 	component: MultiSelect,
 	title: 'MultiSelect'
 } as Meta
 
-const basicOptions: MultiSelectOptions[] = [
+const basicOptions: MultiSelectOption[] = [
 	{ text: 'Lorem', value: '0' },
 	{ text: 'Ipsum', value: '1' },
-	{ text: 'Dolor', value: '3' },
-	{ text: 'Sit', value: '4' },
-	{ text: 'Amet', value: '5' }
+	{ text: 'Dolor', value: '2' },
+	{ text: 'Sit', value: '3' },
+	{ text: 'Amet', value: '4' },
+	{ text: 'Consectetur', value: '5' }
 ]
 
-const Template: Story<MultiSelectProps> = args => <MultiSelect {...args} />
+const ThemedMultiSelect: FC<MultiSelectProps> = (props: MultiSelectProps) => {
+	const theme: SbTheme = useTheme()
+
+	const popupContainerSelector = `.${theme.type}`
+
+	return (
+		<MultiSelect
+			popupContainerSelector={popupContainerSelector}
+			{...props}
+		/>
+	)
+}
+
+const Template: Story<MultiSelectProps> = args => (
+	<ThemedMultiSelect {...args} />
+)
 
 export const Default = Template.bind({})
 Default.args = {
@@ -29,7 +48,6 @@ Default.args = {
 
 export const FullWidth = Template.bind({})
 FullWidth.args = {
-	defaultValues: ['lorem', 'ipsum'],
 	fullWidth: true,
 	options: basicOptions
 }
