7d20ec23f9b52c41ed5941aa0889090eeaf516c0
sam-m-m <sam@dassana.io>
2020-12-20 23:56:17 -0800

    feat #178 - Add Option styles

--- src/components/MultiSelect/utils.ts
+++ src/components/MultiSelect/utils.ts
@@ -29,10 +29,12 @@ export const generateThemedTagStyles = (themeType: ThemeType) => {
 }
 
 const selectPalette = {
-	input: {
-		[dark]: {
-			base: {
-				background: blacks['darken-40'],
+	[dark]: {
+		base: {
+			background: blacks['darken-40']
+		},
+		input: {
+			default: {
 				borderColor: blacks['lighten-20']
 			},
 			disabled: {
@@ -40,24 +42,73 @@ const selectPalette = {
 				borderColor: blacks['darken-20']
 			}
 		},
-		[light]: {
-			base: {
-				background: whites.base,
+		option: {
+			hover: {
+				background: blacks['lighten-10']
+			},
+			selected: {
+				background: blacks['lighten-30'],
+				color: whites.base
+			}
+		}
+	},
+	[light]: {
+		base: {
+			background: whites.base
+		},
+		input: {
+			default: {
 				borderColor: blacks['lighten-80']
 			},
 			disabled: {
 				background: whites.base,
 				borderColor: grays.base
 			}
+		},
+		option: {
+			hover: {
+				background: grays['lighten-40']
+			},
+			selected: {
+				background: grays.base,
+				color: blacks.base
+			}
 		}
 	}
 }
 
 export const generateThemedInputStyles = (themeType: ThemeType) => {
-	const { background, borderColor } = selectPalette.input[themeType].base
+	const { background } = selectPalette[themeType].base
+	const { borderColor } = selectPalette[themeType].input.default
 
 	return {
 		background,
 		borderColor
+	}
+}
+
+export const generateThemedDropdownStyles = (themeType: ThemeType) => {
+	const { background } = selectPalette[themeType].base
+	return { background }
+}
+
+export const generateThemedOptionStyles = (themeType: ThemeType) => {
+	const {
+		base: { color }
+	} = themedStyles[themeType]
+
+	const { hover, selected } = selectPalette[themeType].option
+
+	return {
+		'&.ant-select-item-option': {
+			'&.ant-select-item-option-active': {
+				background: hover.background
+			},
+			'&.ant-select-item-option-selected': {
+				background: selected.background,
+				color: selected.color
+			},
+			color
+		}
 	}
 }
