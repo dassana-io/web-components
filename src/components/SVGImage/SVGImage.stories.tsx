cc3168c80bf5dacec77a11d8a38b748b1691f117
Nancy <68706811+nancy-dassana@users.noreply.github.com>
2021-02-21 21:13:50 -0800

    feat #221, feat #220 - PageLoader and SVGImage Components (#225)
    Closes #221
    
    Closes #220

---
+++ src/components/SVGImage/SVGImage.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { ThemeType } from '../assets/styles'
+import { Meta, Story } from '@storybook/react/types-6-0'
+import { SVGImage, SVGImageProps } from './index'
+import svgImageMap, { ImageTypes } from './fixtures/mockSVGImageMap'
+
+export default {
+	argTypes: {
+		imageMap: { control: { disable: true } },
+		theme: { control: { disable: true } },
+		type: {
+			control: { options: Object.keys(svgImageMap), type: 'select' }
+		}
+	},
+	component: SVGImage,
+	title: 'SVGImage'
+} as Meta
+
+const Template: Story<SVGImageProps<ImageTypes>> = args => (
+	<SVGImage {...args} />
+)
+
+export const Default = Template.bind({})
+
+Default.args = {
+	imageMap: svgImageMap,
+	theme: ThemeType.dark,
+	type: ImageTypes.aws
+}
