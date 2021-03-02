ae835a137031e2ed7f6089a58bf0df57b404cea9
Sam M <68707128+sam-m-m@users.noreply.github.com>
2021-03-02 13:58:59 -0800

    chore #237 - Extract out Tabs and Filter component from web-app-manager (#238)

---
+++ src/__mocks__/filter_mock_data.ts
@@ -0,0 +1,46 @@
+import { FilterOptions } from '../components/Filters'
+
+export const mockDynamicFilterOptions = [
+	{
+		id: '5391d028-d5fe-4c33-a4c9-8719191aaeb1',
+		name: 'Ingestion Dev Account'
+	},
+	{
+		id: '5391d028-d5fe-4c33-a4c9-8719191aaeb8',
+		name: 'Ingestion Prod Account'
+	}
+]
+
+export const mockFilterOptions: FilterOptions = [
+	{
+		filterKey: 'name',
+		options: [
+			'Dev account',
+			'Prod account 1',
+			'Prod account 2',
+			'Test account'
+		],
+		staticFilter: false
+	},
+	{
+		filterKey: 'service',
+		options: [
+			'EC2',
+			'S3',
+			'RDS',
+			'Amazon Keyspaces (for Apache Cassandra)',
+			'EKS'
+		],
+		staticFilter: true
+	},
+	{
+		filterKey: 'model',
+		options: ['instance', 'bucket', 'table'],
+		staticFilter: true
+	},
+	{
+		filterKey: 'region',
+		options: ['us-west-1', 'us-east-2', 'ap-south-1'],
+		staticFilter: true
+	}
+]
---
+++ src/components/Filters/FilterPopover.tsx
@@ -0,0 +1,108 @@
+import { Button } from 'components/Button'
+import { faFilter } from '@fortawesome/free-solid-svg-icons'
+import FilterUnit from './FilterUnit'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'components/Link'
+import { Popover } from 'components/Popover'
+import { SelectOption } from 'components/Select'
+import { usePopoverStyles } from './utils'
+import { FiltersList, OnSearchWrapper, ProcessedFilters } from './types'
+import { IconButton, IconSizes } from 'components/IconButton'
+import React, { FC } from 'react'
+
+interface FilterPopoverProps {
+	allFilters: ProcessedFilters
+	closePopover: () => void
+	dynamicOptions?: SelectOption[]
+	dynamicSearchVal: string
+	filtersList: FiltersList
+	onVisibleChange: (visible: boolean) => void
+	onClickAddFilter: () => void
+	onDelete: (selectedId: string) => void
+	onFilterChange: (selectedId: string, selection: string | string[]) => void
+	onSearchWrapper: OnSearchWrapper
+	pending: boolean
+	setFiltersList: React.Dispatch<React.SetStateAction<FiltersList>>
+	visible?: boolean
+}
+
+export const FilterPopover: FC<FilterPopoverProps> = ({
+	allFilters,
+	closePopover,
+	dynamicOptions,
+	dynamicSearchVal,
+	filtersList,
+	onVisibleChange,
+	onClickAddFilter,
+	onDelete,
+	onFilterChange,
+	onSearchWrapper,
+	pending,
+	setFiltersList,
+	visible = false
+}: FilterPopoverProps) => {
+	const classes = usePopoverStyles()
+
+	return (
+		<Popover
+			classes={[classes.popover]}
+			content={
+				<div className={classes.popoverContent}>
+					<IconButton
+						classes={[classes.closeButton]}
+						onClick={closePopover}
+						size={IconSizes.sm}
+					/>
+					<div className={classes.popoverControls}>
+						<div className={classes.popoverControlsChild}>
+							<FontAwesomeIcon icon={faFilter} />
+						</div>
+						<Button
+							classes={[classes.popoverControlsChild]}
+							disabled={
+								filtersList.length >=
+								Object.keys(allFilters).length
+							}
+							onClick={onClickAddFilter}
+						>
+							+ Add Filter
+						</Button>
+						{filtersList.length > 0 && (
+							<Link
+								classes={[classes.popoverControlsChild]}
+								onClick={() => setFiltersList([])}
+							>
+								Clear Filters
+							</Link>
+						)}
+					</div>
+					<div className={classes.filtersList}>
+						{filtersList.map((filterItem, i) => (
+							<FilterUnit
+								allFilters={allFilters}
+								dynamicOptions={dynamicOptions}
+								dynamicSearchVal={dynamicSearchVal}
+								filtersList={filtersList}
+								id={filterItem.id}
+								key={filterItem.id}
+								onDelete={onDelete}
+								onFilterChange={onFilterChange}
+								onSearchWrapper={onSearchWrapper}
+								pending={pending}
+								selectedFilterKey={filtersList[i].selectedKey}
+							/>
+						))}
+					</div>
+				</div>
+			}
+			onVisibleChange={onVisibleChange}
+			placement='bottomLeft'
+			popupContainerSelector='#filters-popup-wrapper'
+			popupTriggerClasses={[classes.popoverTrigger]}
+			trigger='click'
+			visible={visible}
+		>
+			<i />
+		</Popover>
+	)
+}
