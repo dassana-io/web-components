a8e8799422714dc9a63f67670efc49fbaa5db1ce
Sam M <68707128+sam-m-m@users.noreply.github.com>
2021-03-17 14:06:20 -0700

    feat #231 - Code component (#263)

---
+++ src/components/Code/Code.stories.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Code, CodeProps } from '.'
+import { Meta, Story } from '@storybook/react/types-6-0'
+
+export default {
+	argTypes: {
+		height: { defaultValue: 64 }
+	},
+	component: Code,
+	title: 'Code'
+} as Meta
+
+const Template: Story<CodeProps> = args => <Code {...args} />
+
+/* eslint-disable sort-keys */
+const sampleJSON = {
+	dassana: {
+		context: {
+			attachedResources: {
+				'count-of-attached-eni': 3,
+				'does-any-eni-have-public-ip': true,
+				'are-there-flows-from-internet-in-vpcflows': false
+			}
+		}
+	},
+	resourceConfig: {
+		configuration: {
+			description: 'ssh-from-world',
+			groupName: 'prod-cache',
+			ipPermissions: [
+				{
+					fromPort: 22,
+					ipProtocol: 'tcp',
+					ipv6Ranges: [
+						{
+							cidrIpv6: '::/0'
+						}
+					],
+					prefixListIds: [],
+					toPort: 22,
+					userIdGroupPairs: [],
+					ipv4Ranges: [
+						{
+							cidrIp: '0.0.0.0/0'
+						}
+					]
+				}
+			]
+		}
+	}
+}
+/* eslint-enable sort-keys */
+
+export const Default = Template.bind({})
+Default.args = {
+	code: sampleJSON,
+	search: { placeholder: 'Search...' }
+}
+
+export const HTML = Template.bind({})
+HTML.args = {
+	code: `<div>
+  <p>Lorem Ipsum</p>
+  <span>dolor sit amet</span>
+</div>`,
+	language: 'html'
+}
