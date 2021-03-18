ccd8895758ff8c5ca439533447d9037dc988916b
sam-m-m <sam@dassana.io>
2021-03-18 12:19:59 -0700

    feat #240 - Add Filter stories

---
+++ src/components/Filters/Filters.stories.tsx
@@ -0,0 +1,24 @@
+import { action } from '@storybook/addon-actions'
+import React from 'react'
+import { api as createAxiosInstance, Emitter } from '@dassana-io/web-utils'
+import { Filters, FiltersProps } from '.'
+import { Meta, Story } from '@storybook/react/types-6-0'
+
+export default {
+	argTypes: {
+		onSelectedFiltersChange: {
+			defaultValue: action('onSelectedFiltersChange')
+		}
+	},
+	component: Filters,
+	title: 'Filters'
+} as Meta
+
+const Template: Story<FiltersProps> = args => <Filters {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+	api: createAxiosInstance(''),
+	emitter: new Emitter(),
+	endpoint: 'https://mockendpoint.com'
+}
