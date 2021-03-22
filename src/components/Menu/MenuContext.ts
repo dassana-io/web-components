0fd3dbdecd07c40cad88fd501004d28af72d27b5
Sam M <68707128+sam-m-m@users.noreply.github.com>
2021-03-22 11:00:30 -0700

    chore #264 - Codegen setup (#265)

--- src/components/Menu/MenuContext.ts
+++ src/components/Menu/MenuContext.ts
@@ -6,6 +6,7 @@ export interface MenuContextProps {
 	onClick: (menuKey: Key) => void
 }
 
-export const [useMenuContext, MenuContextProvider] = createCtx<
-	MenuContextProps
->()
+export const [
+	useMenuContext,
+	MenuContextProvider
+] = createCtx<MenuContextProps>()
