ee50c36dd730b6f1241bb2cd971ea539b14c9ca5
mishevong <michelle@dassana.io>
2021-04-14 15:21:07 -0700

    Feat #12 - Add DecoratedBannerStory and test Error stories

--- src/components/Banner/Banner.stories.tsx
+++ src/components/Banner/Banner.stories.tsx
@@ -1,10 +1,17 @@
+import { createUseStyles } from 'react-jss'
 import { ev as NotificationTypes } from '@dassana-io/web-utils'
-import React from 'react'
 import { SecondaryBgDecorator } from '../../../.storybook/utils'
 import { Banner, BannerProps } from '.'
 import { Meta, Story } from '@storybook/react'
+import React, { useState } from 'react'
 
 const { error, info, success, warning } = NotificationTypes
+
+const useStyles = createUseStyles({
+	decorator: {
+		textAlign: 'center'
+	}
+})
 
 export default {
 	argTypes: {
@@ -16,6 +23,37 @@ export default {
 	title: 'Banner'
 } as Meta
 
+const DecoratedBannerStory = (props: BannerProps) => {
+	const classes = useStyles()
+
+	const banners = localStorage.getItem('bannerPref')
+		? JSON.parse(localStorage.getItem('bannerPref')!)
+		: {}
+
+	const [renderBanner, setRenderBanner] = useState<boolean>(
+		banners[props.id] === undefined || banners[props.id] === true
+	)
+
+	const onBannerClose = () => setRenderBanner(false)
+
+	return renderBanner ? (
+		<div onClick={onBannerClose}>
+			<Banner {...props}>
+				Once we receive an alert we scan it against policy risk rules.
+				The rules are evaluated in order from top to bottom. The first
+				rule that matches determines the risk of the alert. Updating a
+				rule will not impact the past alerts. However you can use past
+				alerts as a reference to edit rules such that future alerts get
+				your desired risk classification.
+			</Banner>
+		</div>
+	) : (
+		<div className={classes.decorator}>
+			Closed banner-id: {`${props.id}`}
+		</div>
+	)
+}
+
 const Template: Story<BannerProps> = args => (
 	<Banner {...args}>
 		Once we receive an alert we scan it against policy risk rules. The rules
@@ -26,7 +64,11 @@ const Template: Story<BannerProps> = args => (
 	</Banner>
 )
 
-export const Error = Template.bind({})
+const ErrorTemplate: Story<BannerProps> = args => (
+	<DecoratedBannerStory {...args} />
+)
+
+export const Error = ErrorTemplate.bind({})
 Error.args = {
 	id: 'sb-error-banner',
 	showIcon: true,
