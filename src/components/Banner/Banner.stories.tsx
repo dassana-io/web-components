e7fed6fa7340d07e6d59e1aec231d08f4e07eacd
sam-m-m <sam@dassana.io>
2021-04-15 10:55:19 -0700

    Feat #12 - Refac banner stories

--- src/components/Banner/Banner.stories.tsx
+++ src/components/Banner/Banner.stories.tsx
@@ -1,15 +1,19 @@
+import { Button } from 'components/Button'
 import { createUseStyles } from 'react-jss'
 import { ev as NotificationTypes } from '@dassana-io/web-utils'
+import React from 'react'
 import { SecondaryBgDecorator } from '../../../.storybook/utils'
+import { styleguide } from 'components/assets/styles'
 import { Banner, BannerProps } from '.'
 import { Meta, Story } from '@storybook/react'
-import React, { useState } from 'react'
 
 const { error, info, success, warning } = NotificationTypes
 
+const { spacing } = styleguide
+
 const useStyles = createUseStyles({
-	decorator: {
-		textAlign: 'center'
+	button: {
+		marginTop: spacing.m
 	}
 })
 
@@ -23,23 +27,11 @@ export default {
 	title: 'Banner'
 } as Meta
 
-const DecoratedBannerStory = (props: BannerProps) => {
+const DecoratedBannerStory = ({ id, ...rest }: BannerProps) => {
 	const classes = useStyles()
-
-	const banners = localStorage.getItem('bannerPref')
-		? JSON.parse(localStorage.getItem('bannerPref')!)
-		: {}
-
-	const [renderBanner, setRenderBanner] = useState<boolean>(
-		banners[props.id] === undefined || banners[props.id] === true
-	)
-
-	const onBannerClose = () => setRenderBanner(false)
-
-	// Closed banner is only display for stories
-	return renderBanner ? (
-		<div onClick={onBannerClose}>
-			<Banner {...props}>
+	return (
+		<>
+			<Banner {...rest} id={id}>
 				Once we receive an alert we scan it against policy risk rules.
 				The rules are evaluated in order from top to bottom. The first
 				rule that matches determines the risk of the alert. Updating a
@@ -47,11 +39,18 @@ const DecoratedBannerStory = (props: BannerProps) => {
 				alerts as a reference to edit rules such that future alerts get
 				your desired risk classification.
 			</Banner>
-		</div>
-	) : (
-		<div className={classes.decorator}>
-			Closed banner-id: {`${props.id}`}
-		</div>
+
+			<Button
+				classes={[classes.button]}
+				onClick={() => {
+					localStorage.setItem('bannerPref', JSON.stringify({}))
+
+					window.location.reload()
+				}}
+			>
+				Reset all banner preferences
+			</Button>
+		</>
 	)
 }
 
