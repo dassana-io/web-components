6467db429f94a87869011a37201ad82a937e2d08
sam-m-m <sam@dassana.io>
2021-06-14 10:57:52 -0700

    chore #333 - Extract out PageContainer component from web-apps

---
+++ src/components/PageContainer/index.tsx
@@ -0,0 +1,41 @@
+import cn from 'classnames'
+import { createUseStyles } from 'react-jss'
+import { Helmet } from 'react-helmet'
+import { styleguide } from '../assets/styles'
+import React, { FC, ReactNode } from 'react'
+
+const { spacing } = styleguide
+
+const useStyles = createUseStyles({
+	container: {
+		padding: spacing.l
+	}
+})
+
+export interface PageContainerProps {
+	children: ReactNode
+	classes?: string[]
+	/**
+	 * The HTML document's title that is shown in a browser's title bar or a page's tab
+	 */
+	documentTitle?: string
+}
+
+export const PageContainer: FC<PageContainerProps> = ({
+	children,
+	classes = [],
+	documentTitle = 'Dassana'
+}: PageContainerProps) => {
+	const containerClases = useStyles()
+
+	return (
+		<div className={cn(containerClases.container, classes)}>
+			{documentTitle && (
+				<Helmet>
+					<title>{documentTitle}</title>
+				</Helmet>
+			)}
+			{children}
+		</div>
+	)
+}
