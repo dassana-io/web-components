18b8297ee374e793e7661e6582d982067974048d
mishevong <79538210+mishevong@users.noreply.github.com>
2021-07-09 15:12:45 -0700

    feat #369 - Export scrollOnClick and update stories (#370)
    * feat #369 - move scrollOnClick to utils and update stories

--- src/components/ScrollableSection/ScrollableSection.stories.tsx
+++ src/components/ScrollableSection/ScrollableSection.stories.tsx
@@ -1,6 +1,27 @@
+import { Button } from 'components/Button'
+import { createUseStyles } from 'react-jss'
 import React from 'react'
+import { styleguide } from 'components/assets/styles'
 import { Meta, Story } from '@storybook/react'
 import { ScrollableSection, ScrollableSectionProps } from '.'
+import { ScrollDirections, scrollOnClick } from './utils'
+
+const { flexCenter } = styleguide
+
+const { down } = ScrollDirections
+
+const useStyles = createUseStyles({
+	children: {
+		...flexCenter,
+		height: '100%'
+	},
+	multiContainer: {
+		height: '100vh'
+	},
+	singleContainer: {
+		height: 500
+	}
+})
 
 const sectionsMockData = ['apple', 'pineapple', 'mango']
 
@@ -16,13 +37,118 @@ export default {
 	title: 'ScrollableSection'
 } as Meta
 
-const SingleTemplate: Story<ScrollableSectionProps> = args => (
-	<ScrollableSection {...args}>
-		<div>hello</div>
-	</ScrollableSection>
-)
+const DecoratedSingleTemplate = (args: ScrollableSectionProps) => {
+	const decoratedClasses = useStyles()
 
-export const Single = SingleTemplate.bind({})
+	return (
+		<ScrollableSection
+			{...args}
+			classes={[decoratedClasses.singleContainer]}
+		>
+			<div className={decoratedClasses.children}>
+				<div>pine</div>
+				<div>apple</div>
+			</div>
+		</ScrollableSection>
+	)
+}
+
+export const Single: Story<ScrollableSectionProps> =
+	DecoratedSingleTemplate.bind({})
 Single.args = {
 	name: 'apple'
 }
+
+const multiConfig = [
+	{
+		arrowDown: true,
+		arrowUp: false,
+		name: 'apple',
+		sections: sectionsMockData
+	},
+	{
+		arrowDown: true,
+		arrowUp: true,
+		name: 'pineapple',
+		sections: sectionsMockData
+	},
+	{
+		arrowDown: false,
+		arrowUp: true,
+		name: 'mango',
+		sections: sectionsMockData
+	}
+]
+
+const DecoratedMultipleTemplate = () => {
+	const decoratedClasses = useStyles()
+
+	return (
+		<div>
+			{multiConfig.map((section, i) => (
+				<ScrollableSection
+					classes={[decoratedClasses.multiContainer]}
+					key={i}
+					{...section}
+				>
+					<div className={decoratedClasses.children}>
+						{section.name}
+					</div>
+				</ScrollableSection>
+			))}
+		</div>
+	)
+}
+
+export const Multiple = DecoratedMultipleTemplate.bind({})
+
+const DecoratedScrollTemplate = () => {
+	const decoratedClasses = useStyles()
+
+	const scrollConfig = [
+		{
+			arrowDown: false,
+			arrowUp: false,
+			name: 'apple',
+			render: () => (
+				<div className={decoratedClasses.children}>
+					<Button
+						onClick={() =>
+							scrollOnClick(sectionsMockData, down, 'apple')
+						}
+					>
+						Next Section
+					</Button>
+				</div>
+			),
+			sections: sectionsMockData
+		},
+		{
+			arrowDown: false,
+			arrowUp: true,
+			name: 'pineapple',
+			render: () => (
+				<div className={decoratedClasses.children}>
+					<div>pineapple</div>
+				</div>
+			),
+			sections: sectionsMockData
+		}
+	]
+
+	return (
+		<div className={decoratedClasses.multiContainer}>
+			{scrollConfig.map((args, i) => (
+				<ScrollableSection
+					classes={[decoratedClasses.multiContainer]}
+					key={i}
+					{...args}
+				>
+					{args.render()}
+				</ScrollableSection>
+			))}
+		</div>
+	)
+}
+
+export const ScrollWithoutArrow = DecoratedScrollTemplate.bind({})
--- src/components/ScrollableSection/utils.ts
+++ src/components/ScrollableSection/utils.ts
@@ -1,4 +1,27 @@
+import indexOf from 'lodash/indexOf'
+import { scroller } from 'react-scroll'
+
 export enum ScrollDirections {
 	down = 'down',
 	up = 'up'
+}
+
+const { down, up } = ScrollDirections
+
+export const scrollOnClick = (
+	sections: string[],
+	dir: ScrollDirections,
+	currentSection: string
+) => {
+	let idx = indexOf(sections, currentSection)
+
+	if (dir === up && idx !== 0) {
+		idx -= 1
+	} else if (dir === down && idx < sections.length) {
+		idx += 1
+	}
+
+	scroller.scrollTo(sections[idx], {
+		smooth: true
+	})
 }
