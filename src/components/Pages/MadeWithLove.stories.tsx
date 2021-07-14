c24ad2239bd75f29bcfd4288f5d9fa00b9aa4e67
Sam M <68707128+sam-m-m@users.noreply.github.com>
2021-07-14 16:49:48 -0700

    v0.11.11 -> v0.11.12 (#379)

--- src/components/Pages/MadeWithLove.stories.tsx
+++ src/components/Pages/MadeWithLove.stories.tsx
@@ -1,5 +1,13 @@
-import { MadeWithLove } from './MadeWithLove'
+import { createUseStyles } from 'react-jss'
 import React from 'react'
+import {
+	faAws,
+	faGithub,
+	faLinkedin,
+	faSlack,
+	faTwitterSquare
+} from '@fortawesome/free-brands-svg-icons'
+import { FooterLinksConfig, MadeWithLove } from './MadeWithLove'
 import { Meta, Story } from '@storybook/react'
 
 export default {
@@ -7,6 +15,45 @@ export default {
 	title: 'Made With Love'
 } as Meta
 
-const Template: Story = () => <MadeWithLove />
+const useStyles = createUseStyles({
+	container: {
+		height: '100vh'
+	}
+})
+
+const footerLinksConfig: FooterLinksConfig[] = [
+	{
+		href: 'http://www.twitter.com',
+		icon: faTwitterSquare
+	},
+	{
+		href: 'http://www.linkedin.com',
+		icon: faLinkedin
+	},
+	{
+		href: 'https://github.com/dassana-io/dassana',
+		icon: faGithub
+	},
+	{
+		href: 'https://www.slack.com',
+		icon: faSlack
+	},
+	{
+		href: 'https://github.com/dassana-io/dassana',
+		icon: faAws
+	}
+]
+
+const DecoratedTemplate = () => {
+	const classes = useStyles()
+
+	return (
+		<div className={classes.container}>
+			<MadeWithLove footerLinksConfig={footerLinksConfig} />
+		</div>
+	)
+}
+
+const Template: Story = () => <DecoratedTemplate />
 
 export const WithLove = Template.bind({})
