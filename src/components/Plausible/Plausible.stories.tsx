e7b2ead7786cde356b44f974a5ee9944e432eca2
mishevong <79538210+mishevong@users.noreply.github.com>
2021-07-21 15:57:42 -0700

    feat #396, 400 - Add exclude Plausible and update MadeWithLove link targets (#399)
    * feat #396, 400 - add Plausible and update MadeWithLove link targets

---
+++ src/components/Plausible/Plausible.stories.tsx
@@ -0,0 +1,29 @@
+import { createUseStyles } from 'react-jss'
+import { Plausible } from '.'
+import React from 'react'
+import { Meta, Story } from '@storybook/react'
+
+export default {
+	component: Plausible,
+	title: 'Plausible'
+} as Meta
+
+const useStyles = createUseStyles({
+	container: {
+		height: '100vh'
+	}
+})
+
+const DecoratedTemplate = () => {
+	const decoratedStyles = useStyles()
+
+	return (
+		<div className={decoratedStyles.container}>
+			<Plausible />
+		</div>
+	)
+}
+
+const Template: Story = () => <DecoratedTemplate />
+
+export const DisablePlausible = Template.bind({})
---
+++ src/components/Plausible/index.tsx
@@ -0,0 +1,86 @@
+import { Button } from 'components/Button'
+import { createUseStyles } from 'react-jss'
+import { styleguide } from 'components/assets/styles'
+import React, { FC, useState } from 'react'
+
+const {
+	colors: { blacks, blues, grays, greens, reds },
+	flexCenter,
+	flexDown,
+	fontWeight,
+	spacing
+} = styleguide
+
+interface ReactJssProps {
+	isExcluded: boolean
+}
+
+const useStyles = createUseStyles({
+	btn: {
+		backgroundColor: `${blues.base} !important`,
+		borderColor: `${blues.base} !important`,
+		color: `${grays.base} !important`
+	},
+	container: {
+		...flexCenter,
+		...flexDown,
+		backgroundColor: blacks.base,
+		color: grays.base,
+		height: '100%',
+		textAlign: 'center'
+	},
+	desc: {
+		padding: `${spacing.xl}px 0`
+	},
+	exclude: {
+		color: ({ isExcluded }: ReactJssProps) =>
+			isExcluded ? greens.base : reds.base,
+		fontWeight: fontWeight.regular * 2,
+		padding: `0 ${spacing.xs}px`
+	},
+	status: {
+		paddingBottom: spacing['m+'],
+		paddingTop: spacing.s
+	},
+	title: {
+		fontSize: spacing.l * 2,
+		fontWeight: fontWeight.bold
+	}
+})
+
+export const Plausible: FC = () => {
+	const [isExcluded, setIsExcluded] = useState(localStorage.plausible_ignore)
+	const classes = useStyles({ isExcluded })
+
+	const toggleExclusion = () => {
+		const exclusionState = localStorage.plausible_ignore === 'true'
+
+		if (exclusionState) {
+			setIsExcluded(false)
+			delete localStorage.plausible_ignore
+		} else {
+			setIsExcluded(true)
+			localStorage.plausible_ignore = 'true'
+		}
+	}
+
+	return (
+		<div className={classes.container}>
+			<div className={classes.title}>Plausible Exclude</div>
+			<div className={classes.desc}>
+				Click the button below to toggle your exclusion in analytics for
+				this site
+				<div className={classes.status}>
+					You currently
+					<span className={classes.exclude}>
+						{isExcluded ? 'are' : 'are not'}
+					</span>
+					excluding your visits.
+				</div>
+			</div>
+			<Button classes={[classes.btn]} onClick={toggleExclusion}>
+				{isExcluded ? 'Stop excluding my visits' : 'Exclude my visits'}
+			</Button>
+		</div>
+	)
+}
