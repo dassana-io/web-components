6eedade8116bce7be01eb68c3983c3b1fb17e3b7
Sam M <68707128+sam-m-m@users.noreply.github.com>
2021-07-26 13:52:26 -0700

    feat #378 - Update made with love links (#417)

--- src/components/Pages/MadeWithLove.stories.tsx
+++ src/components/Pages/MadeWithLove.stories.tsx
@@ -1,13 +1,6 @@
 import { createUseStyles } from 'react-jss'
+import { MadeWithLove } from './MadeWithLove'
 import React from 'react'
-import {
-	faAws,
-	faGithub,
-	faLinkedin,
-	faSlack,
-	faTwitterSquare
-} from '@fortawesome/free-brands-svg-icons'
-import { FooterLinksConfig, MadeWithLove } from './MadeWithLove'
 import { Meta, Story } from '@storybook/react'
 
 export default {
@@ -21,35 +14,12 @@ const useStyles = createUseStyles({
 	}
 })
 
-const footerLinksConfig: FooterLinksConfig[] = [
-	{
-		href: 'http://www.twitter.com',
-		icon: faTwitterSquare
-	},
-	{
-		href: 'http://www.linkedin.com',
-		icon: faLinkedin
-	},
-	{
-		href: 'https://github.com/dassana-io/dassana',
-		icon: faGithub
-	},
-	{
-		href: 'https://www.slack.com',
-		icon: faSlack
-	},
-	{
-		href: 'https://github.com/dassana-io/dassana',
-		icon: faAws
-	}
-]
-
 const DecoratedTemplate = () => {
 	const classes = useStyles()
 
 	return (
 		<div className={classes.container}>
-			<MadeWithLove footerLinksConfig={footerLinksConfig} />
+			<MadeWithLove />
 		</div>
 	)
 }
