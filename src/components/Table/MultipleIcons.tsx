b957a69b92e60f7b2fd3b2144e96c91517f9d1a6
sam-m-m <sam@dassana.io>
2021-08-02 16:06:19 -0700

    Write codez

--- src/components/Table/MultipleIcons.tsx
+++ src/components/Table/MultipleIcons.tsx
@@ -3,30 +3,53 @@ import { createUseStyles } from 'react-jss'
 import { styleguide } from 'components/assets/styles'
 import { Tooltip } from 'components/Tooltip'
 import { useTableContext } from './TableContext'
-import { Icon, IconProps } from '../Icon'
-import React, { FC } from 'react'
+import React, { FC, ReactNode } from 'react'
 
 const { spacing } = styleguide
 
-export const defaultIconHeight = 25
+const iconSpacing = spacing.s
 
 const useStyles = createUseStyles({
-	count: { borderBottom: '1px solid', marginLeft: spacing.s },
+	count: { borderBottom: '1px solid', marginLeft: iconSpacing },
 	icon: {
 		'&:not(:last-child)': {
-			marginRight: spacing.s
+			marginRight: iconSpacing
 		}
 	},
 	iconInTooltip: {
-		marginBottom: spacing.s,
-		marginRight: spacing.s
+		marginBottom: iconSpacing
 	},
 	tooltip: {
 		'&.ant-tooltip': {
 			'& > .ant-tooltip-content > .ant-tooltip-inner': {
-				padding: `${spacing.s}px 0 0 ${spacing.s}px`
+				display: 'flex',
+				flexWrap: 'wrap',
+				padding: `${iconSpacing}px 0 0 ${iconSpacing}px`
 			},
-			maxWidth: ({ isMobile }) => (isMobile ? 140 : 250 - spacing.s)
+			width: ({ height, isMobile, truncatedIconsLength }) => {
+				/* Since the width of icon is same as height, use height
+        to calculate total width of the Tooltip inside which icons
+        render. NOTE: This is optimized for icons with tooltips. */
+				const totalWidth =
+					height * truncatedIconsLength +
+					iconSpacing * truncatedIconsLength +
+					iconSpacing
+
+				const defaultWidth = height * 6 + iconSpacing * 6 + iconSpacing
+				const mobileWidth = height * 4 + iconSpacing * 4 + iconSpacing
+
+				let width = mobileWidth
+
+				if (totalWidth < mobileWidth) {
+					width = totalWidth
+				} else if (isMobile) {
+					width = mobileWidth
+				} else {
+					width = defaultWidth
+				}
+
+				return width
+			}
 		}
 	},
 	wrapper: {
@@ -36,52 +59,51 @@ const useStyles = createUseStyles({
 })
 
 interface Props {
-	iconPropsArr: IconProps[]
-	height?: number
+	height: number
+	icons: ReactNode[]
 	truncateLength?: number
 }
 
 export const MultipleIcons: FC<Props> = ({
-	iconPropsArr = [],
-	height = defaultIconHeight,
+	height,
+	icons = [],
 	truncateLength = 2
 }: Props) => {
 	const { isMobile } = useTableContext()
 
-	const classes = useStyles({ isMobile })
+	const classes = useStyles({
+		height,
+		isMobile,
+		truncatedIconsLength: icons.length - truncateLength
+	})
 
 	interface RenderIcons {
 		sliceStartIndex: number
 		sliceEndIndex?: number
 		isInsideTooltip?: boolean
 	}
+
 	const renderIcons = ({
 		sliceStartIndex,
 		sliceEndIndex,
 		isInsideTooltip
 	}: RenderIcons) =>
-		iconPropsArr
-			.slice(sliceStartIndex, sliceEndIndex)
-			.map((iconProps, i) => {
-				return (
-					<Icon
-						{...iconProps}
-						classes={[
-							cn({
-								[classes.icon]: true,
-								[classes.iconInTooltip]: isInsideTooltip
-							})
-						]}
-						height={height}
-						key={i}
-					/>
-				)
-			})
+		icons.slice(sliceStartIndex, sliceEndIndex).map((iconComp, i) => (
+			<span
+				className={cn({
+					[classes.icon]: true,
+					[classes.iconInTooltip]: isInsideTooltip
+				})}
+				key={i}
+			>
+				{iconComp}
+			</span>
+		))
 
 	return (
 		<div className={classes.wrapper}>
 			{renderIcons({ sliceEndIndex: truncateLength, sliceStartIndex: 0 })}
-			{truncateLength < iconPropsArr.length && (
+			{truncateLength < icons.length && (
 				<Tooltip
 					classes={[classes.tooltip]}
 					placement='bottom'
@@ -100,7 +122,7 @@ export const MultipleIcons: FC<Props> = ({
 							e.stopPropagation()
 						}
 					>
-						+{iconPropsArr.length - truncateLength}
+						+{icons.length - truncateLength}
 					</span>
 				</Tooltip>
 			)}
