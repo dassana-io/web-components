6af8939590a83931d3798e371f5fff32c4352588
Nancy <68706811+nancy-dassana@users.noreply.github.com>
2021-10-14 16:57:49 -0700

    v0.11.44 -> v0.12.0
    feat #502 - Infotip Component (#503)
    
    Closes #502

---
+++ src/components/InfoTip/index.tsx
@@ -0,0 +1,53 @@
+import { createUseStyles } from 'react-jss'
+import { faInfoCircle } from '@fortawesome/pro-light-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { FC } from 'react'
+import { themes, ThemeType } from '../assets/styles'
+import { Tooltip, TooltipProps } from 'components/Tooltip'
+
+const { dark, light } = ThemeType
+
+const useStyles = createUseStyles({
+	infoIcon: {
+		color: themes[light].text.primary
+	},
+	// eslint-disable-next-line sort-keys
+	'@global': {
+		[`.${dark}`]: {
+			'& $infoIcon': { color: themes[dark].text.primary }
+		}
+	}
+})
+
+export interface InfoTipProps {
+	classes: TooltipProps['classes']
+	content: TooltipProps['title']
+	infoTipTriggerClasses: TooltipProps['tooltipTriggerClasses']
+	placement: TooltipProps['placement']
+	popupContainerSelector: TooltipProps['popupContainerSelector']
+}
+
+export const InfoTip: FC<InfoTipProps> = ({
+	classes = [],
+	infoTipTriggerClasses = [],
+	content,
+	placement = 'top',
+	popupContainerSelector
+}: InfoTipProps) => {
+	const componentClasses = useStyles()
+
+	return (
+		<Tooltip
+			classes={classes}
+			placement={placement}
+			popupContainerSelector={popupContainerSelector}
+			title={content}
+			tooltipTriggerClasses={infoTipTriggerClasses}
+		>
+			<FontAwesomeIcon
+				className={componentClasses.infoIcon}
+				icon={faInfoCircle}
+			/>
+		</Tooltip>
+	)
+}
