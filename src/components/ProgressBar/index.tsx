959fa2a94a5759dc21c63c4a59303873f744a37d
Jasper <97872491+jasper-dassana@users.noreply.github.com>
2022-03-05 03:48:58 -0600

    Mark `classes` prop optional

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -74,7 +74,7 @@ const useStyles = createUseStyles({
 /* eslint-enable quotes */
 
 export interface ProgressBarProps {
-	classes: string[]
+	classes?: string[]
 }
 
 export const ProgressBar: FC<ProgressBarProps> = ({
