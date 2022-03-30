2f1764926724a659f62bb85500e03cfeef5f513f
Nancy Lai <nancy@dassana.io>
2022-03-29 22:33:41 -0700

    Add tab util functions

--- src/components/Tabs/utils.ts
+++ src/components/Tabs/utils.ts
@@ -1,3 +1,5 @@
+import findIndex from 'lodash/findIndex'
+import { TabConfig } from './index'
 import { themedStyles, themes, ThemeType } from 'components/assets/styles'
 
 export const generateThemedTabsListStyles = (themeType: ThemeType) => {
@@ -18,4 +20,23 @@ export const generateThemedActiveTabStyles = (themeType: ThemeType) => {
 		borderBottomColor: active,
 		color: active
 	}
+}
+
+export enum TabParams {
+	page = 'page'
+}
+
+export const findDefaultActiveIndex = (
+	key: TabParams,
+	params: Record<string, string[]>,
+	tabConfig: TabConfig[],
+	defaultIndex = 0
+): number => {
+	let index = defaultIndex
+
+	if (params[key]) {
+		index = findIndex(tabConfig, { key: params[key][0] })
+	}
+
+	return index
 }
