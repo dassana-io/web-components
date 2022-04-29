237ba823991447c5aa6f8280ca3fe238cf9ceac0
Jasper <97872491+jasper-dassana@users.noreply.github.com>
2022-04-29 09:58:00 -0700

    Underline markdown links

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -18,6 +18,10 @@ const useStyles = createUseStyles({
 	'@global': {
 		'.markdown-body': {
 			...generateThemedMarkdownStyles(light),
+			'& a': {
+				color: 'unset',
+				textDecoration: 'underline'
+			},
 			'& code': {
 				...generateThemedMarkdownCodeStyles(light),
 				color: 'unset',
