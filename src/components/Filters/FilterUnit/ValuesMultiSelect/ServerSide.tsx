17414cd13ea39dd43abecea7226c5a5c08650866
Nancy <68706811+nancy-dassana@users.noreply.github.com>
2022-09-14 19:02:15 -0700

    fix #735 - [Filters V1] Fix server side options not populating on first load (#736)

--- src/components/Filters/FilterUnit/ValuesMultiSelect/ServerSide.tsx
+++ src/components/Filters/FilterUnit/ValuesMultiSelect/ServerSide.tsx
@@ -100,6 +100,7 @@ export const ServerSideValuesMS: FC<Props> = ({
 										[...filterOptValues],
 										!!optionsConfig
 									),
+									...dynamicOptions,
 									...selectedValues
 								],
 								'value'
