18016008f3dff82faa86a1b21680e9abe11e97b1
Nancy Lai <nancy@dassana.io>
2022-10-26 15:13:49 -0700

    enhancement #759 - [Banner] Update banner for use with preferences

--- src/components/Banner/utils.ts
+++ src/components/Banner/utils.ts
@@ -1,4 +1,3 @@
-import isUndefined from 'lodash/isUndefined'
 import { themedStyles, ThemeType } from 'components/assets/styles'
 
 export const generateThemedBannerStyles = (themeType: ThemeType) => {
@@ -11,28 +10,3 @@ export const generateThemedBannerStyles = (themeType: ThemeType) => {
 		color
 	}
 }
-
-export const BANNER_PREFERENCE = 'bannerPref'
-
-export interface Banners {
-	[key: string]: boolean
-}
-
-export const getBannerPreferences = (): Banners => {
-	const bannerPref = localStorage.getItem(BANNER_PREFERENCE)
-
-	return bannerPref ? JSON.parse(bannerPref) : {}
-}
-
-export const isNewBanner = (banners: Banners, id: string): boolean =>
-	isUndefined(banners[id])
-
-export const updateBannerPreferences = (
-	currentBanners: Banners,
-	id: string,
-	visible: boolean
-) =>
-	localStorage.setItem(
-		BANNER_PREFERENCE,
-		JSON.stringify({ ...currentBanners, [id]: visible })
-	)
