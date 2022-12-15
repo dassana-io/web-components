000c65ebcf5b03a783126f5bea276231ae83a7c5
Jasper <97872491+jasper-dassana@users.noreply.github.com>
2022-12-14 17:31:39 -0800

    enhancement #766 - RadioButtonGroup disabled state and infotip (#767)
    * RadioButtonGroup disabled state and infotip
    
    * 0.13.27
    
    * Update snapshots

--- src/components/RadioButtonGroup/index.tsx
+++ src/components/RadioButtonGroup/index.tsx
@@ -2,16 +2,24 @@ import 'antd/lib/radio/style/index.css'
 import { CommonComponentProps } from '../types'
 import { createUseStyles } from 'react-jss'
 import { generateRadioButtonGroupStyles } from './utils'
+import { InfoTip } from 'components/InfoTip'
 import RadioButtonGroupSkeleton from './RadioButtonGroupSkeleton'
 import { SizeType } from 'antd/lib/config-provider/SizeContext'
+import { styleguide } from 'components/assets/styles'
 import { ThemeType } from '../assets/styles/themes'
+import { TooltipProps } from 'components/Tooltip'
 import { Radio as AntDRadio, RadioChangeEvent } from 'antd'
 import { getDataTestAttributeProp, TAG } from '../utils'
 import React, { FC, ReactNode } from 'react'
 
 const { dark, light } = ThemeType
 
+const { spacing } = styleguide
+
 const useStyles = createUseStyles({
+	infoTip: {
+		marginLeft: spacing.s
+	},
 	radioGroup: generateRadioButtonGroupStyles(light),
 	// eslint-disable-next-line sort-keys
 	'@global': {
@@ -29,6 +37,11 @@ export interface RadioButtonGroupOptions {
 
 export type RadioChangeEventHandler = (e: RadioChangeEvent) => void
 
+interface InfoTipConfig {
+	message: ReactNode
+	placement: TooltipProps['placement']
+}
+
 export interface RadioButtonGroupProps extends CommonComponentProps {
 	/**
 	 * Default value for radio group. Without this, the first option will be defaulted
@@ -39,6 +52,7 @@ export interface RadioButtonGroupProps extends CommonComponentProps {
 	 * @default false
 	 */
 	disabled?: boolean
+	infoTipConfig?: InfoTipConfig
 	/**
 	 * Whether or not to show skeleton loader
 	 * @default false
@@ -63,6 +77,7 @@ export const RadioButtonGroup: FC<RadioButtonGroupProps> = ({
 	defaultValue,
 	dataTag,
 	disabled = false,
+	infoTipConfig,
 	loading = false,
 	onChange,
 	options,
@@ -83,16 +98,25 @@ export const RadioButtonGroup: FC<RadioButtonGroupProps> = ({
 	return loading ? (
 		<RadioButtonGroupSkeleton count={options.length} />
 	) : (
-		<AntDRadio.Group
-			{...rest}
-			buttonStyle='solid'
-			className={classes.radioGroup}
-			defaultValue={defaultValue || options[0].value}
-			disabled={disabled}
-			name={getDataTestAttributeProp('radioGroup', dataTag)[TAG]}
-			optionType='button'
-			options={options}
-			{...controlledCmpProps}
-		/>
+		<>
+			<AntDRadio.Group
+				{...rest}
+				buttonStyle='solid'
+				className={classes.radioGroup}
+				defaultValue={defaultValue || options[0].value}
+				disabled={disabled}
+				name={getDataTestAttributeProp('radioGroup', dataTag)[TAG]}
+				optionType='button'
+				options={options}
+				{...controlledCmpProps}
+			/>
+			{infoTipConfig && (
+				<InfoTip
+					content={infoTipConfig.message}
+					infoTipTriggerClasses={[classes.infoTip]}
+					placement={infoTipConfig.placement}
+				/>
+			)}
+		</>
 	)
 }
