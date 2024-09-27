fa1f6261ae5bfed006cea618bdfe43d589ca4c97
Nancy Lai <nancy@dassana.io>
2024-09-26 22:28:43 -0700

    Upgrade storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,22 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-actions', '@storybook/addon-mdx-gfm'],
-  framework: {
-    name: '@storybook/react-vite',
-    options: {}
-  },
-  docs: {
-    autodocs: 'tag'
-  }
-};
-export default config;
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-actions',
+		'@chromatic-com/storybook'
+	],
+
+	framework: {
+		name: '@storybook/react-vite',
+		options: {}
+	},
+
+	docs: {},
+
+	typescript: {
+		reactDocgen: 'react-docgen-typescript'
+	}
+}
+export default config
