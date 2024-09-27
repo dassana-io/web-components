42636e1d3cee1c2da181bd2f7933f8b9d99ac01e
Nancy Lai <nancy@dassana.io>
2024-09-26 19:42:32 -0700

    Try to fix chromatic error

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,13 +1,16 @@
 import commonjs from '@rollup/plugin-commonjs'
 import fs from 'fs'
 import image from '@rollup/plugin-image'
+import json from '@rollup/plugin-json'
 import path from 'path'
-import pkg from './package.json'
+import pkg from './package.json' assert { type: 'json' }
 import resolve from '@rollup/plugin-node-resolve'
 import styles from 'rollup-plugin-styles'
 import svgr from '@svgr/rollup'
 import terser from '@rollup/plugin-terser'
-import typescript from 'rollup-plugin-typescript2'
+import typescript from '@rollup/plugin-typescript'
+
+import { fileURLToPath } from 'url'
 
 const assetFileNames = '[name]-[hash][extname]'
 
@@ -18,7 +21,10 @@ https://github.com/rollup/rollup/issues/558#issuecomment-353797769
 const rootImport = options => ({
 	resolveId: importee => {
 		if (importee[0] === '/') {
+			const __filename = fileURLToPath(import.meta.url) // get the resolved path to the file
+			const __dirname = path.dirname(__filename) // get the name of the directory
 			const rootPath = `${options.root}${importee}.*`
+
 			const absPath = path.resolve(__dirname, rootPath)
 
 			return fs.existsSync(absPath) ? absPath : null
@@ -68,6 +74,7 @@ export default {
 		'uuid'
 	],
 	input: 'src/components/index.ts',
+	preserveModules: true,
 	output: [
 		{
 			file: pkg.main,
@@ -87,11 +94,17 @@ export default {
 		resolve(),
 		commonjs(),
 		styles(),
+		json(),
 		svgr(),
 		image(),
 		typescript({
-			tsconfig: 'tsconfig.rollup.json',
-			useTsconfigDeclarationDir: true
+			include: ['src/components/**/*', 'src/custom.d.ts'],
+			exclude: [
+				'**/__tests__',
+				'**/fixtures',
+				'*.stories.tsx',
+				'.storybook'
+			]
 		}),
 		terser()
 	]
