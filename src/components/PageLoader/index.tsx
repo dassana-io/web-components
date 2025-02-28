999262ed0a1ab3b4f1b879a6eac71121854b1bb3
Jasper <97872491+jasper-dassana@users.noreply.github.com>
2025-02-28 14:34:59 -0600

    enhancement #969 - Replace loader with Deepwatch logo

--- src/components/PageLoader/index.tsx
+++ src/components/PageLoader/index.tsx
@@ -1,11 +1,8 @@
 import cn from 'classnames'
 import { createUseStyles } from 'react-jss'
+import DWLoader from '../assets/images/deepwatch_loader.gif'
 import { faCircleNotch } from '@fortawesome/pro-light-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { ReactComponent as LoaderPhase0 } from '../assets/images/loader_phase_0.svg'
-import { ReactComponent as LoaderPhase1 } from '../assets/images/loader_phase_1.svg'
-import { ReactComponent as LoaderPhase2 } from '../assets/images/loader_phase_2.svg'
-import { ReactComponent as LoaderPhase3 } from '../assets/images/loader_phase_3.svg'
 import { styleguide } from '../assets/styles'
 import React, { type ReactNode } from 'react'
 
@@ -20,47 +17,13 @@ const useStyles = createUseStyles({
 		background: 'transparent',
 		width: 'inherit'
 	},
-
 	iconWrapper: {
 		height: 100,
 		position: 'relative',
 		width: 100
 	},
-	loader1: { animation: 'loader1 3s infinite' },
-	loader2: { animation: 'loader2 3s infinite' },
-	loader3: { animation: 'loader3 3s infinite' },
-	// eslint-disable-next-line sort-keys
-	'@global': {
-		'@keyframes loader1': {
-			'0%, 24%': {
-				opacity: 0
-			},
-			'25%, 49%': {
-				opacity: 1
-			},
-			'50%, 100%': {
-				opacity: 0
-			}
-		},
-		'@keyframes loader2': {
-			'0%, 49%': {
-				opacity: 0
-			},
-			'50%, 74%': {
-				opacity: 1
-			},
-			'75%, 100%': {
-				opacity: 0
-			}
-		},
-		'@keyframes loader3': {
-			'0%, 74%': {
-				opacity: 0
-			},
-			'75%, 100%': {
-				opacity: 1
-			}
-		}
+	loader: {
+		width: 100
 	}
 })
 
@@ -88,12 +51,11 @@ export const PageLoader: React.FC<PageLoaderProps> = ({
 			{generic ? (
 				<FontAwesomeIcon icon={faCircleNotch} size='5x' spin />
 			) : (
-				<div className={compClasses.iconWrapper}>
-					<LoaderPhase0 />
-					<LoaderPhase1 className={compClasses.loader1} />
-					<LoaderPhase2 className={compClasses.loader2} />
-					<LoaderPhase3 className={compClasses.loader3} />
-				</div>
+				<img
+					alt='Deepwatch loader'
+					className={compClasses.loader}
+					src={DWLoader}
+				/>
 			)}
 		</div>
 	)
